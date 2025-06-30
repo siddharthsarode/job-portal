55febae9027afb3f20245dae9aff58a2f969dc46
Siddharth Sarode <SiddharthSarode279@gmail.com>
2025-06-30 16:52:59 +0530

    change index.js file

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import userRouter from "./router/userRouter.js";
 
 dotenv.config();
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
 connectDB();
