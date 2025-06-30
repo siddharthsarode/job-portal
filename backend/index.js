404061d74a8df94e2c76a94573d197b4272a9d84
Siddharth Sarode <SiddharthSarode279@gmail.com>
2025-06-30 18:04:20 +0530

    change index

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,12 @@ const PORT = process.env.PORT || 3000;
 // Connect to MongoDB
 connectDB();
 
-app.use(cors({ origin: `${process.env.FRONTEND_URL}`, credentials: true }));
+app.use(
+  cors({
+    origin: "https://job-portal-ome5.onrender.com",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
